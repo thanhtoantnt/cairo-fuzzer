e80828ef578f294f53d25bd08b0d2606c53d279c
thanhtoantnt <thanhtoantnt@gmail.com>
2023-10-05 10:44:05 +0800

    simplify the implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{fs, process};
-
 use clap::Parser;
+use std::process;
 
 mod cli;
 mod custom_rand;
@@ -15,23 +14,8 @@ use fuzzer::fuzzer::Fuzzer;
 
 use log::error;
 fn main() {
-    let header = r###"
-                 _______  _______ _________ _______  _______         _______           _______  _______  _______  _______ 
-                (  ____ \(  ___  )\__   __/(  ____ )(  ___  )       (  ____ \|\     /|/ ___   )/ ___   )(  ____ \(  ____ )
-                | (    \/| (   ) |   ) (   | (    )|| (   ) |       | (    \/| )   ( |\/   )  |\/   )  || (    \/| (    )|
-                | |      | (___) |   | |   | (____)|| |   | | _____ | (__    | |   | |    /   )    /   )| (__    | (____)|
-                | |      |  ___  |   | |   |     __)| |   | |(_____)|  __)   | |   | |   /   /    /   / |  __)   |     __)
-                | |      | (   ) |   | |   | (\ (   | |   | |       | (      | |   | |  /   /    /   /  | (      | (\ (   
-                | (____/\| )   ( |___) (___| ) \ \__| (___) |       | )      | (___) | /   (_/\ /   (_/\| (____/\| ) \ \__
-                (_______/|/     \|\_______/|/   \__/(_______)       |/       (_______)(_______/(_______/(_______/|/   \__/"###;
-    println!("\t=========================================================================================================================");
-    println!("{}", header);
-    println!("\n\t=========================================================================================================================");
-    // get cli args
     let opt = Opt::parse();
-    // create config file
-    let mut config = match opt.config {
-        // config file provided
+    let config = match opt.config {
         Some(config_file) => Config::load_config(&config_file),
         None => {
             if opt.contract.len() == 0 && opt.proptesting == false {
@@ -63,35 +47,8 @@ fn main() {
             }
         }
     };
-    //if proptesting enabled
-    if config.proptesting {
-        let contents = fs::read_to_string(&config.contract_file).unwrap();
-        println!("\t\t\t\t\t\t\tSearching for Fuzzing functions ...");
-        let functions = json::json_parser::get_proptesting_functions(&contents);
-        if functions.len() == 0 {
-            println!("\t\t\t\t\t\t\t!! No Fuzzing functions found !!");
-            return;
-        }
-        for func in functions {
-            println!("\n\t\t\t\t\t\t\tFunction found => {}", &func);
-            config.function_name = func;
-            let mut fuzzer = Fuzzer::new(&config);
-            println!(
-                "\t\t\t\t\t\t\t=== {} === is now running for {} iterations",
-                config.function_name, config.iter
-            );
-            fuzzer.fuzz();
-        }
-    } else {
-        // create the fuzzer
-        let mut fuzzer = Fuzzer::new(&config);
 
-        // replay, minimizer mode
-        if opt.replay || opt.minimizer {
-            fuzzer.replay();
-        // launch fuzzing
-        } else {
-            fuzzer.fuzz();
-        }
-    }
+    let mut fuzzer = Fuzzer::new(&config);
+
+    fuzzer.fuzz();
 }
